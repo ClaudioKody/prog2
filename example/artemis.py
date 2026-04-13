f64eb96c1567e1f4b9a712b50c855fefb9c322de
Claudio <claudiokody@gmail.com>
2026-04-13 16:57:44 -0300

    asd

---
+++ example/artemis.py
@@ -0,0 +1,27 @@
+#Clase Padre
+class Vehiculo:
+    def __init__(self, marca, modelo, nombre):
+        self.marca = marca
+        self.modelo = modelo
+        self.nombre = nombre
+    
+    def encender(self):
+        return f"{self.nombre} encendido"
+    
+    def info(self):
+        return f"{self.nombre} - {self.marca} {self.modelo}"
+
+#Clase Hija
+class Cohete(Vehiculo):
+    def __init__(self, marca, modelo, nombre, mision):
+        super().__init__(marca, modelo, nombre)
+        self.mision = mision
+    
+    def lanzar(self):
+        return f"{self.nombre} despegando en la misión {self.mision}"
+    
+    def estacionar(self):
+        return f"{self.nombre} acoplado en la estación espacial"
+    
+    def explotar(Self):
+        return f"{self.nombre} Explotando capsula" 
